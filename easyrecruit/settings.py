89a10d1f15b6229a9574cabd438c0f1aaf694433
Naman Kumar <namant744@gmail.com>
2021-03-31 18:43:33 +0530

    frontend changed and ckeditor added

--- easyrecruit/settings.py
+++ easyrecruit/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'accounts.apps.AccountsConfig',
-    'jobportal.apps.JobportalConfig'
+    'jobportal.apps.JobportalConfig',
+    'ckeditor',
 
 ]
 
